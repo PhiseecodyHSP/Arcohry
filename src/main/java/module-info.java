4a506cc02f627bf9b57d73ba1fe6a77d94e7dc33
HSP <a18307775247@163.com>
2026-06-19 23:34:13 +0800

    移除了部分 requires 和 opens

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,11 @@
 module io.github.phiseecodyhsp.arcstory {
     requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires javafx.base;
     requires java.desktop;
-    requires jdk.unsupported.desktop;
     requires org.jetbrains.annotations;
     requires javafx.media;
     requires com.fasterxml.jackson.databind;
 
-    opens io.github.phiseecodyhsp.arcstory to javafx.fxml;
-    opens io.github.phiseecodyhsp.arcstory.storage to javafx.fxml;
-    opens io.github.phiseecodyhsp.arcstory.storyMode to javafx.fxml, com.fasterxml.jackson.databind;
+    opens io.github.phiseecodyhsp.arcstory.storyMode to com.fasterxml.jackson.databind;
     exports io.github.phiseecodyhsp.arcstory;
     exports io.github.phiseecodyhsp.arcstory.storyMode;
     exports io.github.phiseecodyhsp.arcstory.storage;
