fa08c6458607e802f7fec25764a80b827b8c700c
yang_12345 <1716437252@qq.com>
2026-06-21 22:05:57 +0800

    新增: 部分注释

--- src/main/java/io/github/phiseecodyhsp/arcstory/res/ResourceLoader.java
+++ src/main/java/io/github/phiseecodyhsp/arcstory/res/ResourceLoader.java
@@ -11,6 +11,57 @@ import java.net.URL;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 资源管理实用类.
+ *
+ * <p>该类提供了一些<b>加载 resources 资源文件夹内资源</b>的方法, 便于管理各种<b>图片</b>,
+ * <b>音频</b>等资源.
+ *
+ * <h2>资源映射表</h2>
+ *
+ * <p><b>资源映射表</b>是一个 <b>JSON 文件</b>, 位于
+ * {@code resources/io/github/phiseecodyhsp/arcstory/resource-config.json}.
+ * 其内部结构为 <b>"分类 -> 键值对"</b>:
+ *
+ *  <pre>{@code
+ *  {
+ *      "images": {
+ *          "hikari": "images/hikari.png",
+ *          "tairitsu": "images/tairitsu.png"
+ *      },
+ *      "audios": {
+ *          "bgm": "audios/bgm.mp3"
+ *      }
+ *  }
+ *  }</pre>
+ *
+ *  <p>读取时, {@code images} 和 {@code audios} 为<b>分类</b>, 其中的<b>键值对</b>会被记录.
+ *  我们推荐使用 <b>lower_snake_case</b> 来命名分类和键.
+ *  键值对的值是一个 {@code resources/io/github/phiseecodyhsp/arcstory/} 下的文件路径.
+ *
+ *  <p>使用 {@link #resolvePath(String, String)} 可以根据<b>分类和键名</b>来获取映射的资源路径.
+ *  在键名确定后, 要更改资源<b>仅需调整资源映射表</b>, 无需修改源代码.
+ *
+ *  <h2>资源读取与缓存</h2>
+ *
+ *  <p>{@link ResourceLoader} 提供了一些方法读取并创建<b>资源对象</b>, 如 {@link #loadImage(String)}.
+ *  一般搭配 {@link #resolvePath(String, String)} 来使用这些方法:
+ *
+ *  <pre>{@code
+ *  Image image = ResourceLoader.loadImage(ResourceLoader.resolvePath("images", "hikari"));
+ *  }</pre>
+ *
+ *  <p>这些资源对象被创建后, 会<b>被缓存</b>进一个 {@link Map} 对象中, 后续重复读取时会<b>复用该对象</b>.
+ *
+ *  <p>使用 {@link #clearCaches()} 来<b>清空缓存</b>, 通常<b>不会</b>随意调用.
+ *
+ *  <h2>线程安全</h2>
+ *
+ *  <p>{@link ResourceLoader} 是<b>线程安全</b>的, 这意味着多个线程<b>可以同时</b>通过此类读取资源,
+ *  <b>多线程同时</b>获取某一资源也<b>不会</b>创建多份缓存.
+ *
+ * @author KashiKoiAstra
+ */
 public final class ResourceLoader {
 
     private static final String BASE = "/io/github/phiseecodyhsp/arcstory/";
@@ -44,6 +95,13 @@ public final class ResourceLoader {
         return config;
     }
 
+    /**
+     * 从资源映射表中读取资源路径.
+     *
+     * @param category 资源分类
+     * @param key 键
+     * @return 资源映射表中对应的路径, 不存在时返回 {@code null}
+     */
     public static String resolvePath(String category, String key) {
         JsonNode node = config().path(category).path(key);
         if (node.isTextual()) {
--- src/main/java/io/github/phiseecodyhsp/arcstory/ui/util/JavaFxUtil.java
+++ src/main/java/io/github/phiseecodyhsp/arcstory/ui/util/JavaFxUtil.java
@@ -6,6 +6,11 @@ public final class JavaFxUtil {
 
     private JavaFxUtil() {}
 
+    /**
+     * 在 JavaFX 线程上运行一段代码. 若当前已在 JavaFX 线程上, 则直接运行.
+     *
+     * @param action 要运行的代码
+     */
     public static void runOnFxThread(Runnable action) {
         if (Platform.isFxApplicationThread()) {
             action.run();
