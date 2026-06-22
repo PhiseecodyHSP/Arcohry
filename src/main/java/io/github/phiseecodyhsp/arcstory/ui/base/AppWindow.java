fd8aefc1a87137bf5dcc04a2c224f2f550ecf31b
yang_12345 <1716437252@qq.com>
2026-06-22 15:25:26 +0800

    修复: 窗口缩放问题

--- src/main/java/io/github/phiseecodyhsp/arcstory/ui/base/AppWindow.java
+++ src/main/java/io/github/phiseecodyhsp/arcstory/ui/base/AppWindow.java
@@ -1,17 +1,16 @@
 package io.github.phiseecodyhsp.arcstory.ui.base;
 
 import io.github.phiseecodyhsp.arcstory.util.ScreenMetrics;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
-import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 public class AppWindow {
 
     private final Stage stage;
     private final StackPane root;
-    private final Scale scaleTransform;
     private final ScreenManager screenManager;
 
     private static final double ASPECT_RATIO = 16.0 / 9.0;
@@ -19,11 +18,9 @@ public class AppWindow {
     public AppWindow(Stage stage) {
         this.stage = stage;
         this.root = new StackPane();
-        this.scaleTransform = new Scale();
-        this.root.getTransforms().add(this.scaleTransform);
         this.screenManager = new ScreenManager(this.root);
 
-        double width = ScreenMetrics.getPrimaryScreenWidth() * 0.5;
+        double width = 0.5D * ScreenMetrics.getPrimaryScreenWidth();
         double height = width / ASPECT_RATIO;
 
         Scene scene = new Scene(this.root, width, height);
@@ -45,24 +42,11 @@ public class AppWindow {
     }
 
     private void updateScale() {
-        double sceneW = this.stage.getScene().getWidth();
-        double sceneH = this.stage.getScene().getHeight();
-
-        double scaleX = sceneW / this.root.prefWidth(-1);
-        double scaleY = sceneH / this.root.prefHeight(-1);
-        double scale = Math.min(scaleX, scaleY);
-
-        if (this.root.prefWidth(-1.0D) <= 0) {
-            scale = Math.min(sceneW / 1280.0D, sceneH / 720.0D);
-        }
-
-        this.scaleTransform.setX(scale);
-        this.scaleTransform.setY(scale);
-        this.scaleTransform.setPivotX(0.0D);
-        this.scaleTransform.setPivotY(0.0D);
-
-        this.root.setTranslateX((sceneW - this.root.prefWidth(-1.0D) * scale) / 2.0D);
-        this.root.setTranslateY((sceneH - this.root.prefHeight(-1.0D) * scale) / 2.0D);
+        Rectangle2D screenBounds = ScreenMetrics.getPrimaryScreenBounds();
+        double scale = Math.max(this.stage.getScene().getWidth() / screenBounds.getWidth(),
+                this.stage.getScene().getHeight() / screenBounds.getHeight());
+        this.root.setScaleX(scale);
+        this.root.setScaleY(scale);
     }
 
     public Stage getStage() {
