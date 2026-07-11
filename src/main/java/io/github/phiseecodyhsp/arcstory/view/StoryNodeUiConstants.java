2861a5e048ba18ef4b870f7b97b634b133eae174
HSP <a18307775247@163.com>
2026-07-12 00:23:36 +0800

    style: 添加了注释

--- src/main/java/io/github/phiseecodyhsp/arcstory/view/StoryNodeUiConstants.java
+++ src/main/java/io/github/phiseecodyhsp/arcstory/view/StoryNodeUiConstants.java
@@ -12,10 +12,14 @@ public final class StoryNodeUiConstants {
     public static final double BORDER_WIDTH;
     public static final double BORDER_ARC_SIZE;
 
+    public static final double TEXT_PLAYER_FONT_SIZE;
+
     static {
-        SIDE_LENGTH = ScreenMetrics.SCREEN_WIDTH * 0.1D;
+        SIDE_LENGTH = ScreenMetrics.SCREEN_WIDTH * 0.05D;
         DIAGONAL_LENGTH = SIDE_LENGTH * MathUtil.SQRT_2;
         BORDER_WIDTH = 2.0D;
         BORDER_ARC_SIZE = 5.0D;
+
+        TEXT_PLAYER_FONT_SIZE = DIAGONAL_LENGTH / 5.0D;
     }
 }
