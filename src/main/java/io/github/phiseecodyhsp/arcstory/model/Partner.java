9a14948987b6ba1736a2b6285d65aaaae7207b2a
HSP <a18307775247@163.com>
2026-07-12 06:03:30 +0800

    refactor: 修改注释

--- src/main/java/io/github/phiseecodyhsp/arcstory/model/Partner.java
+++ src/main/java/io/github/phiseecodyhsp/arcstory/model/Partner.java
@@ -1,14 +1,14 @@
 package io.github.phiseecodyhsp.arcstory.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.phiseecodyhsp.arcstory.res.ResourceLocation;
-import org.jetbrains.annotations.NotNull;
 
 /**
- * Arcaea 搭档.
+ * 由 JSON 数据驱动的 Arcaea 搭档.
  *
  * @param name 搭档名称
  * @param avatarLocation 搭档头像路径
  * @param illustrationLocation 搭档立绘路径
  */
-public record Partner(String name, @NotNull ResourceLocation avatarLocation, @NotNull ResourceLocation illustrationLocation) {
+public record Partner(@JsonProperty String name, @JsonProperty ResourceLocation avatarLocation, @JsonProperty ResourceLocation illustrationLocation) {
 }
