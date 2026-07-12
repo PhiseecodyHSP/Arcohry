1c7a2e4813af0eaa3f757ef8200be5d535f55984
HSP <a18307775247@163.com>
2026-07-12 21:55:07 +0800

    feat: 添加了一个外发光 DropShadow

--- src/main/java/io/github/phiseecodyhsp/arcstory/view/Effects.java
+++ src/main/java/io/github/phiseecodyhsp/arcstory/view/Effects.java
@@ -14,4 +14,9 @@ public class Effects {
      * 按钮背景阴影. 由于按钮被旋转了 45 度, 这里需要单独写一个阴影.
      */
     public static final DropShadow ROTATED_SHADOW = new DropShadow(15.0D, 10.0D, 3.0D, new Color(0, 0, 0, 0.5));
+
+    /**
+     * 外发光
+     */
+    public static final DropShadow OUTER_GLOW = new DropShadow(10.0D, Color.WHITE);
 }
